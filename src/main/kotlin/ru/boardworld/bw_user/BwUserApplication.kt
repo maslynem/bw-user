350ac77f7df08379afd023bed783ebc95d89c49a
svitaly <vit-1998@inbox.ru>
2025-09-25 09:16:05 +0300

    Initial commit

---
+++ src/main/kotlin/ru/boardworld/bw_user/BwUserApplication.kt
@@ -0,0 +1,11 @@
+package ru.boardworld.bw_user
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BwUserApplication
+
+fun main(args: Array<String>) {
+	runApplication<BwUserApplication>(*args)
+}
---
+++ src/test/kotlin/ru/boardworld/bw_user/BwUserApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.boardworld.bw_user
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BwUserApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
